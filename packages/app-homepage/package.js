0864a0e7eb01e5d4267d1e6e54657b54ea0354c7
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-06-11 18:23:45 -0400

    First crack at testing with only native dependencies

--- packages/app-homepage/package.js
+++ packages/app-homepage/package.js
@@ -8,9 +8,19 @@ Package.onUse(function(api) {
   api.use([
     'less',
     'templating',
-    'spacebars'
+    'spacebars',
+    'blaze'
   ], 'client');
 
   api.addFiles('home.less', 'client');
   api.addFiles('home.html', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use([
+    'jquery',
+    'app:homepage',
+    'tinytest@1.0.0'
+  ], 'client');
+  api.addFiles('app-homepage-test.js', 'client');
 });
