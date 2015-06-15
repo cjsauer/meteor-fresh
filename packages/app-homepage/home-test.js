3f835137ee2274b020176109eb197c043474b985
Calvin Sauer <calvin.j.sauer@gmail.com>
2015-06-15 17:56:48 -0400

    Homepage test text is more descriptive

--- packages/app-homepage/home-test.js
+++ packages/app-homepage/home-test.js
@@ -1,4 +1,4 @@
-Tinytest.add('Home template', function(test) {
+Tinytest.add('Homepage renders the welcome message', function(test) {
   var dom = $(renderToDiv(Template.home));
   test.equal(dom.find('.fresh').text(), 'Enjoy your fresh start.');
 });
